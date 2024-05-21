454f8de5cebc1a9fa02118b4827cce014c9fa59f
ZippyCreative <75967689+ZippyCreative@users.noreply.github.com>
2024-05-21 16:28:47 +0530

    DropBoxAdded

---
+++ Assets/Editor/PostBuildScript.cs
@@ -0,0 +1,40 @@
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Diagnostics;
+using System.IO;
+
+public class PostBuildScript
+{
+    [PostProcessBuild]
+    public static void OnPostProcessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        string dropboxToken = System.Environment.GetEnvironmentVariable("DROPBOX_ACCESS_TOKEN");
+        if (string.IsNullOrEmpty(dropboxToken))
+        {
+            UnityEngine.Debug.LogError("Dropbox access token is not set.");
+            return;
+        }
+
+        string dropboxPath = "/path/in/dropbox/build.zip";
+        string localFilePath = pathToBuiltProject;
+
+        Process process = new Process();
+        process.StartInfo.FileName = "curl";
+        process.StartInfo.Arguments = $"-X POST https://content.dropboxapi.com/2/files/upload " +
+                                      $"--header \"Authorization: Bearer {dropboxToken}\" " +
+                                      $"--header \"Dropbox-API-Arg: {{\\\"path\\\": \\\"{dropboxPath}\\\", \\\"mode\\\": \\\"add\\\", \\\"autorename\\\": true, \\\"mute\\\": false, \\\"strict_conflict\\\": false}}\" " +
+                                      $"--header \"Content-Type: application/octet-stream\" " +
+                                      $"--data-binary @{localFilePath}";
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.Start();
+
+        string output = process.StandardOutput.ReadToEnd();
+        string error = process.StandardError.ReadToEnd();
+        process.WaitForExit();
+
+        UnityEngine.Debug.Log("Dropbox Upload Output: " + output);
+        UnityEngine.Debug.LogError("Dropbox Upload Error: " + error);
+    }
+}
